9fbdc8769355a58588cfbe0fefde2eb602b5a844
Takobz <kobelakhutso.kk@gmail.com>
2024-05-03 07:02:18 +0200

    add test projects into the solution

---
+++ src/groceries-web-api/Groceries.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Groceries.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/groceries-web-api/Groceries.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Groceries.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}

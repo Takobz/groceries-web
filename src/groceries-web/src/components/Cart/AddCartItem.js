b3e8963acf43d579954501e547c9d084dc637563
takobz <kobelakhutso.kk@gmail.com>
2024-08-29 23:46:06 +0200

    fix: make modal visible on button click - not done

--- src/groceries-web/src/components/Cart/AddCartItem.js
+++ src/groceries-web/src/components/Cart/AddCartItem.js
@@ -1,12 +1,39 @@
-import { Modal } from "@mui/material";
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 
 const AddCartItem = (props) => {
 
     return (
         <Modal
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
             open={props.isOpen}
             onClose={props.closeModal}>
-            <p>Hi</p>
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Some Text Here
+                </Typography>
+            </Box>
         </Modal>
     )
 }
